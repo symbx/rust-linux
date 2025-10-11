93d44acfe2936143e45795742e56a22e1da472d6
Martin Atkins <mart@degeneration.co.uk>
2025-10-11 08:25:31 -0700

    linux-io: Some ioctl reqs for pseudoterminal controllers

--- linux-io/src/tty.rs
+++ linux-io/src/tty.rs
@@ -1,4 +1,9 @@
-use crate::fd::ioctl::{ioctl_read, ioctl_write, IoctlReqRead, IoctlReqWrite};
+use linux_unsafe::{int, ulong, ushort};
+
+use crate::fd::ioctl::{
+    ioctl_read, ioctl_write, ioctl_writeread, IoctlReqRead, IoctlReqWrite, IoctlReqWriteRead, _IOR,
+    _IOW,
+};
 
 /// `ioctl` request for retrieving the current window size of a tty.
 // NOTE: This ioctl number isn't valid for all Linux architectures, but is valid
@@ -16,19 +21,42 @@ pub const TIOCSWINSZ: IoctlReqWrite<TtyDevice, WindowSize> = unsafe { ioctl_writ
 #[repr(C)]
 pub struct WindowSize {
     /// The number of rows in the window.
-    pub ws_row: linux_unsafe::ushort,
+    pub ws_row: ushort,
 
     /// The number of columns in the window.
-    pub ws_col: linux_unsafe::ushort,
+    pub ws_col: ushort,
 
     /// Not actually used.
-    pub ws_xpixel: linux_unsafe::ushort,
+    pub ws_xpixel: ushort,
 
     /// Not actually used.
-    pub ws_ypixel: linux_unsafe::ushort,
+    pub ws_ypixel: ushort,
 }
 
 /// A marker type for [`super::File`] objects that represent tty devices.
 pub struct TtyDevice;
 
 impl super::fd::ioctl::IoDevice for TtyDevice {}
+
+/// A marker type for [`super::File`] objects that represent pseudoterminal
+/// controllers, as are typically opened from `/dev/ptmx`.
+pub struct PtyControllerDevice;
+
+impl super::fd::ioctl::IoDevice for PtyControllerDevice {}
+
+const IOCTL_TTY: ulong = b'T' as ulong;
+
+/// `ioctl` request for setting or removing the lock on the terminal device
+/// associated with a [`PtyControllerDevice`].
+// NOTE: This ioctl number isn't valid for all Linux architectures, but is valid
+// for all of the ones linux-unsafe supoorts at the time of writing.
+pub const TIOCSPTLCK: IoctlReqWrite<PtyControllerDevice, int> =
+    unsafe { ioctl_write(_IOW(IOCTL_TTY, 0x31, core::mem::size_of::<int>() as ulong)) };
+
+/// `ioctl` request for finding the device unit number for a pseudoterminal.
+/// Use this with a [`PtyControllerDevice`] to determine which node to open
+/// from the `/dev/pts` directory.
+// NOTE: This ioctl number isn't valid for all Linux architectures, but is valid
+// for all of the ones linux-unsafe supoorts at the time of writing.
+pub const TIOCGPTN: IoctlReqWriteRead<PtyControllerDevice, int> =
+    unsafe { ioctl_writeread(_IOR(IOCTL_TTY, 0x30, core::mem::size_of::<int>() as ulong)) };
